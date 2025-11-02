4ef8ca25b7a7901ca9855d6ed833fd2e2a422606
Claipousse <clementconrie@gmail.com>
2025-11-02 11:35:54 +0100

    Creation projet - ajout config.py & database.py

---
+++ backend/app/config.py
@@ -0,0 +1,27 @@
+'''
+Centre de configuration de l'app, tout est centralisé et facile à modifier via le .env
+Le fichier .env est lu en priorité par Pydantic pour importer les variables nécéssaires (DATABASE_URL, DEBUG, ...)
+Cela permet de pouvoir changer de config (dev/test/prod), sans devoir toucher au code et seulement via le .env
+Cependant, les variables dans ce fichiers servent si jamais il n'y a pas de .env fournit, pour avoir des valeurs par défauts
+'''
+from pathlib import Path
+from pydantic_settings import BaseSettings #Sert à gérer la configuration via variables d'environnements/.env par exemple
+
+#Chemin jusqu'a la racine du projet
+BASE_DIR =  Path(__file__).resolve().parent.parent.parent #3x parent pour remonter à la racine
+
+class Settings(BaseSettings): #Hérite de BaseSettings (Pydantic)
+    DATABASE_URL: str = f"sqlite:///{BASE_DIR}/data/ledgerone.db" #Chemin vers la BDD
+    DEBUG: bool = True #Debug On/Off, en prod penser à mettre False
+    ALLOWED_ORIGINS: list[str] = ["http://localhost:8000", "http://127.0.0.1:8000"] #CROS, autorise front à communiquer avec l'api
+
+    #Config de l'API
+    API_PREFIX: str = "/api"
+    PROJECT_NAME: str = "LedgerOne API"
+    VERSION: str = "1.0.0" #v1 pour le moment, si jamais on fait des patch/mises à jour on peut mettre 1.0.1 par exemple
+
+    class Config:
+        env_file = ".env" #Fichier à lire pour changer les variables
+        case_sensitive = True #Sensible à la casse
+
+settings = Settings() #Pydantic lit le .env et charge les valeurs
---
+++ backend/app/database.py
@@ -0,0 +1,34 @@
+'''
+Sert à gérer & établir la connexion à la base de donnée
+Evite qu'on doive le faire à chaque fichier, et donc évite les répétitions, un import suffit
+'''
+#SQL Alchemy
+from sqlalchemy import create_engine #Sert à créer la connexion à la bdd
+from sqlalchemy.ext.declarative import declarative_base #Crée la classe de base de tous les modèles
+from sqlalchemy.orm import sessionmaker #Crée une session (= conversation avec une bdd en gros)
+
+#On veut récup des infos de config.py
+from app.config import settings
+
+#Création de l'engine (= moteur de connexion à la base de données)
+engine = create_engine(
+    settings.DATABASE_URL, #Se connecte au fichier SQLite situé sur ce chemin
+    connect_args={"check_same_thread" : False}, #Permet d'avoir plusieurs requêtes en même temps
+    echo=settings.DEBUG #On reprends les paramètres de debug de config (True/false)
+)
+
+#On initialise la session
+SessionLocal = sessionmaker(
+    autocommit=False, #Pas de sauvegarde auto, on le fait à la fin pour éviter les mauvaises manip et pouvoir rollback
+    autoflush=False, #On décide quand synchroniser (=préparer) les objets python & la DB
+    bind=engine #On utilise le moteur de connexion crée au dessus
+)
+
+Base = declarative_base() #Classe mère pour les modèles
+
+def get_db():
+    db = SessionLocal() #Ouvre session
+    try:
+        yield db
+    finally:
+        db.close() #On ferme la session à la fin si pas déjà fait
