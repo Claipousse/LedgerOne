d038feadcc63f26ba615a03226013a2fc302b38f
Claipousse <clementconrie@gmail.com>
2025-11-04 14:15:30 +0100

    Ajout schemas/__init__.py -> /schemas terminé

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,32 @@
+'''
+Package Schémas - Centralise tous les schémas Pydantic
+Permet d'importer facilement tous les schémas depuis 1 seul endroit
+'''
+
+from app.schemas.category import (
+    CategoryBase,
+    CategoryCreate,
+    CategoryUpdate,
+    CategoryResponse
+)
+
+from app.schemas.transaction import (
+    TransactionBase,
+    TransactionCreate,
+    TransactionUpdate,
+    TransactionInResponse,
+    TransactionResponse
+)
+
+from app.schemas.settings (
+    SettingsBase,
+    SettingsUpdate,
+    SettingsResponse
+)
+
+#On liste tous les schémas dispo
+__all__ = [
+    "CategoryBase", "CategoryCreate", "CategoryUpdate", "CategoryResponse",
+    "TransactionBase", "TransactionCreate", "TransactionUpdate", "TransactionInResponse", "TransactionResponse",
+    "SettingsBase", "SettingsUpdate", "SettingsResponse",
+]
